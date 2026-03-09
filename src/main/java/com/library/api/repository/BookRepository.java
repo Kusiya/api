a1c5fb8027763c2899dff5a6fd78e53c8a121cae
Kusiya <kerrin2025@gmail.com>
2026-03-09 20:59:20 +0400

    Интерфейс репозитория

--- src/main/java/com/library/api/repository/BookRepository.java
+++ src/main/java/com/library/api/repository/BookRepository.java
@@ -10,4 +10,5 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByAvailableTrue();
     List<Book> findByTitleContainingIgnoreCase(String title);
     boolean existsByIsbn(String isbn);
+
 }
